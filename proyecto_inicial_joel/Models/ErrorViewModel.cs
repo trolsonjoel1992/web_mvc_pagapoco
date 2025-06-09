3ae6129af1333330b043ea680b527f5ee0158954
Trolsonjoel1992 <joelutn@gmail.com>
2025-06-09 06:48:39 -0300

    Add project files.

---
+++ proyecto_inicial_joel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace proyecto_inicial_joel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
